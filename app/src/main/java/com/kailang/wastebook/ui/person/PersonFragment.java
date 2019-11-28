08c3be297d181576c1da2515a3a6b04472cdb9f9
kailanguage <41792700+kailanguage@users.noreply.github.com>
2019-11-28 15:19:24 +0800

    Fix Budget Bug

--- app/src/main/java/com/kailang/wastebook/ui/person/PersonFragment.java
+++ app/src/main/java/com/kailang/wastebook/ui/person/PersonFragment.java
@@ -1,6 +1,7 @@
 package com.kailang.wastebook.ui.person;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -12,9 +13,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -30,25 +33,31 @@ import java.util.List;
 
 public class PersonFragment extends Fragment {
 
+    private final static String YEAR_BUDGET="year_budget_total",MONTH_BUDGET="month_budget_total";
     private PersonViewModel personViewModel;
     private SharedPreferences shp;
     private TextView tv_total_wastebook,tv_year_budget_total,tv_month_budget_total,tv_year_budget_left,tv_month_budget_left,tv_logout;
-    private EditText et_year_budget_total,et_month_budget_total;
+    private TextView et_year_budget_total,et_month_budget_total;
+    private String thisYear,thisMonth;
+    private double yearTotal=0.0,monthTotal=0.0;
+    private int y_b_shp,m_b_shp;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         personViewModel =
                 ViewModelProviders.of(this).get(PersonViewModel.class);
+
+        thisYear=DateToLongUtils.getSysYear();
+        thisMonth=DateToLongUtils.getSysMonth();
+
         View root = inflater.inflate(R.layout.fragment_person, container, false);
         tv_total_wastebook=root.findViewById(R.id.tv_total_wastebook);
 
         tv_year_budget_total=root.findViewById(R.id.textView_year_budget);
-        tv_year_budget_total.setText("设置"+DateToLongUtils.getSysYear()+"年预算");
+        tv_year_budget_total.setText("设置"+thisYear+"年预算");
         tv_month_budget_total=root.findViewById(R.id.textView_month_budget);
-        tv_month_budget_total.setText("设置"+DateToLongUtils.getSysMonth()+"月预算");
+        tv_month_budget_total.setText("设置"+thisMonth+"月预算");
         et_year_budget_total=root.findViewById(R.id.et_year_budget_total);
-        et_year_budget_total.addTextChangedListener(yearWatcher);
         et_month_budget_total=root.findViewById(R.id.et_month_budget_total);
-        et_month_budget_total.addTextChangedListener(monthWatcher);
         tv_year_budget_left=root.findViewById(R.id.tv_year_budget_left);
         tv_month_budget_left=root.findViewById(R.id.tv_month_budget_left);
 
@@ -65,55 +74,103 @@ public class PersonFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        shp = requireActivity().getSharedPreferences("budget", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = shp.edit();
+        if(shp.getAll()!=null){
+            y_b_shp= shp.getInt(YEAR_BUDGET,0);
+            if(y_b_shp!=0){
+                et_year_budget_total.setText(y_b_shp+"元");
+            }
+             m_b_shp=shp.getInt(MONTH_BUDGET,0);
+            if(m_b_shp!=0){
+                et_month_budget_total.setText(m_b_shp+"元");
+            }
+        }
+
         personViewModel.getAllWasteBookLive().observe(getViewLifecycleOwner(), new Observer<List<WasteBook>>() {
             @Override
             public void onChanged(List<WasteBook> wasteBooks) {
                 tv_total_wastebook.setText(wasteBooks.size()+"条记录");
+                if(wasteBooks!=null) {
+                    yearTotal=0.0;monthTotal=0.0;
+                    for(WasteBook w:wasteBooks) {
+                        String yearStart = thisYear + "-01-01 00:00:00";
+                        String monthStart = thisYear + "-" + thisMonth + "-1 00:00:00";
+                        if (w.getTime()>=DateToLongUtils.dateToLong(yearStart)){
+                            if(w.isType())
+                            yearTotal+=w.getAmount();
+                            if(w.getTime()>=DateToLongUtils.dateToLong(monthStart)){
+                                if(w.isType())
+                                    monthTotal+=w.getAmount();
+                            }
+                        }
+                    }
+                    if(y_b_shp!=0&&m_b_shp!=0){
+                        tv_year_budget_left.setText((y_b_shp-yearTotal)+"元");
+                        tv_month_budget_left.setText((m_b_shp-monthTotal)+"元");
+                    }
+                }
             }
         });
 
 
+        et_year_budget_total.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText input = new EditText(getActivity());
+                input.setText(et_year_budget_total.getText());
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("添加年预算").setView(input)
+                        .setNegativeButton(R.string.cancel, null);
+                builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        String tmp;
+                        tmp =input.getText().toString().trim();
+                        try{
 
-        shp = requireActivity().getSharedPreferences("budget", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = shp.edit();
-        String tmpYear=et_year_budget_total.getText().toString();
+                            if(!tmp.isEmpty()){
+                                et_year_budget_total.setText(Integer.parseInt(input.getText().toString())+"元");
+                                editor.putInt(YEAR_BUDGET,Integer.parseInt(input.getText().toString()));
+                                editor.commit();
+                            }
+                        }catch (Exception e){
+                            Toast.makeText(getContext(),"请输入合法信息",Toast.LENGTH_SHORT);
+                        }
 
-        if(tmpYear!=null&&!tmpYear.isEmpty()){
-            editor.putInt("year_budget_total",Integer.parseInt(tmpYear));
-            editor.commit();
-        }
+                    }
+                }).show();
+            }
+        });
+
+        et_month_budget_total.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText input = new EditText(getActivity());
+                input.setText(et_month_budget_total.getText());
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("添加月预算").setView(input)
+                        .setNegativeButton(R.string.cancel, null);
+                builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        String tmp;
+                        tmp =input.getText().toString().trim();
+                        try{
+
+                            if(!tmp.isEmpty()){
+                                et_month_budget_total.setText(Integer.parseInt(input.getText().toString())+"元");
+                                editor.putInt(MONTH_BUDGET,Integer.parseInt(input.getText().toString()));
+                                editor.commit();
+                            }
+                        }catch (Exception e){
+                            Toast.makeText(getContext(),"请输入合法信息",Toast.LENGTH_SHORT);
+                        }
+                    }
+                }).show();
+
+
+            }
+        });
     }
 
-    private TextWatcher yearWatcher = new TextWatcher() {
-        @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-        }
-
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-        }
-
-        @Override
-        public void afterTextChanged(Editable s) {
-            Log.e("xxxx",s.toString());
-        }
-    };
-    private TextWatcher monthWatcher = new TextWatcher() {
-        @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-        }
-
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-        }
-
-        @Override
-        public void afterTextChanged(Editable s) {
-            Log.e("xxxx",s.toString());
-        }
-    };
 }
